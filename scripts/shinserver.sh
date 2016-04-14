921bcd3e1fb896f466c349d8865300ac9433a2c1
Andrzej <a.jedrzejewski@kainos.com>
2016-04-14 10:16:21 +0200

    add scripts with statistic software in provisioning

--- scripts/shinserver.sh
+++ scripts/shinserver.sh
@@ -1,3 +1,11 @@
 cd
+
+echo "Installing Shiny in progress............"
+sudo su - -c "R -e \"install.packages('shiny', repos = 'http://cran.rstudio.com/')\""
+
+echo "Installing gdebi-core in progress............"
+apt-get install gdebi-core -y
+
+echo "Installing ShinyServer in progress............"
 wget -O shiny-server.deb http://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.3.0.403-amd64.deb
 gdebi -n shiny-server.deb
