03ccd4c2cb69ee42fec8ba89a93409edf6db091a
Andrzej <a.jedrzejewski@kainos.com>
2016-04-14 10:57:04 +0200

    update iso image to 14.04.4

--- scripts/shinyserver.sh
+++ scripts/shinyserver.sh
@@ -9,3 +9,12 @@ apt-get install gdebi-core -y
 echo "Installing ShinyServer in progress............"
 wget -O shiny-server.deb http://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.3.0.403-amd64.deb
 gdebi -n shiny-server.deb
+
+sudo groupadd shiny-apps
+sudo usermod -aG shiny-apps ubuntu
+sudo usermod -aG shiny-apps vagrant
+sudo usermod -aG shiny-apps shiny
+cd /srv/shiny-server
+sudo chown -R ubuntu:shiny-apps .
+sudo chmod g+w .
+sudo chmod g+s .
